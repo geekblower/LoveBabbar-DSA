bad5ec93f23a055c928755562e6bc1a3c7bd62f7
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-12-24 21:12:24 +0530

    Lecture - 02 (23/12/2022)

---
+++ Lecture - 02 - Write your first program in C++/3_Typecasting.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a = 'a';
+    cout << a << endl;
+
+    char ch = 98;
+    cout << ch << endl;
+
+    char ch1 = 123456;
+    cout << ch1 << endl;
+    
+}
