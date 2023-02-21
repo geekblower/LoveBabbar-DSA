8130245e9f3d04d25c581098d598f9b720288e26
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-02-21 20:36:34 +0530

    Update 08_Rotate_Image.cpp

--- Lecture-023/08_Rotate_Image.cpp
+++ Lecture-023/08_Rotate_Image.cpp
@@ -11,7 +11,7 @@ void rotateMatrix(vector<vector<int>> &arr) {
             temp[j][col-i-1] = arr[i][j];
         }
     }
-// 1 4 7 11 15 2 5 8 12 19 3 6 9 16 22 10 13 14 17 24 18 21 23 26 30
+
     arr = temp;
 }
 
