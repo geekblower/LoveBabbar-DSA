ddb22172c97ad38fcff06d95aa6b2893d23ed977
HardikJainGit <133627261+HardikJainGit@users.noreply.github.com>
2023-12-17 23:22:02 +0530

    Update 1_Rat_In_A_Maze.cpp
    Added about popping back during backtracking

--- Lecture-082/1_Rat_In_A_Maze.cpp
+++ Lecture-082/1_Rat_In_A_Maze.cpp
@@ -3,39 +3,49 @@
 using namespace std;
 
 class Solution {
-  void findPathHelper(int i, int j, vector < vector < int >> & a, int n, vector < string > & ans, string move,
+  void findPathHelper(int i, int j, vector < vector < int >> & a, int n, vector < string > & ans, string &move,
     vector < vector < int >> & vis) {
     if (i == n - 1 && j == n - 1) {
       ans.push_back(move);
       return;
     }
 
+    //DLRU Lexographic order
+
     // downward
     if (i + 1 < n && !vis[i + 1][j] && a[i + 1][j] == 1) {
       vis[i][j] = 1;
-      findPathHelper(i + 1, j, a, n, ans, move + 'D', vis);
+      move.push_back('D');
+      findPathHelper(i + 1, j, a, n, ans, move, vis);
       vis[i][j] = 0;
+      move.pop_back(); //either write this or pass the move string without by ref
     }
 
     // left
     if (j - 1 >= 0 && !vis[i][j - 1] && a[i][j - 1] == 1) {
       vis[i][j] = 1;
-      findPathHelper(i, j - 1, a, n, ans, move + 'L', vis);
+      move.push_back('L');
+      findPathHelper(i, j - 1, a, n, ans, move, vis);
       vis[i][j] = 0;
+      move.pop_back();
     }
 
     // right 
     if (j + 1 < n && !vis[i][j + 1] && a[i][j + 1] == 1) {
       vis[i][j] = 1;
-      findPathHelper(i, j + 1, a, n, ans, move + 'R', vis);
+      move.push_back('R');
+      findPathHelper(i, j + 1, a, n, ans, move, vis);
       vis[i][j] = 0;
+      move.pop_back();
     }
 
     // upward
     if (i - 1 >= 0 && !vis[i - 1][j] && a[i - 1][j] == 1) {
       vis[i][j] = 1;
-      findPathHelper(i - 1, j, a, n, ans, move + 'U', vis);
+      move.push_back('U');
+      findPathHelper(i - 1, j, a, n, ans, move, vis);
       vis[i][j] = 0;
+      move.pop_back();
     }
 
   }
@@ -43,16 +53,17 @@ class Solution {
     vector < string > findPath(vector < vector < int >> & m, int n) {
       vector < string > ans;
       vector < vector < int >> vis(n, vector < int > (n, 0));
+      string s="";
 
-      if (m[0][0] == 1) findPathHelper(0, 0, m, n, ans, "", vis);
+      if (m[0][0] == 1) findPathHelper(0, 0, m, n, ans, s, vis);
       return ans;
     }
 };
 
-int main() {
-  int n = 3;
+int main() { 
+  int n = 4;
 
-   vector < vector < int >> m = {{1,1,1},{1,0,1},{1,1,1}};
+   vector < vector < int >> m = {{1,0,0,0},{1,1,0,1},{1,1,0,0},{0,1,1,1}};
 
   Solution obj;
   vector < string > result = obj.findPath(m, n);
